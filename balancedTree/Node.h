cc00c2a2608c40f9a8d809ba0b01651559dd2315
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-05-10 09:25:09 -0400

    node class

---
+++ balancedTree/Node.h
@@ -0,0 +1,25 @@
+//
+//  Node.h
+//  balancedTree
+//
+//  Created by Rosnel Leyva-Cortés on 5/10/21.
+//
+
+#ifndef Node_h
+#define Node_h
+
+@interface Node : NSObject
+
+@property Node * prevParent;
+
+@property BOOL red;
+
+@property int storedValue;
+
+-(instancetype)init;
+
+-(instancetype)initWithVal:(int)value;
+
+@end
+
+#endif /* Node_h */
---
+++ balancedTree/Node.m
@@ -0,0 +1,31 @@
+//
+//  Node.m
+//  balancedTree
+//
+//  Created by Rosnel Leyva-Cortés on 5/10/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "Node.h"
+
+@implementation Node
+
+-(instancetype)init{
+    if(self){
+        self = [super init];
+        self.red = true;
+        self.storedValue = 0;
+    }
+    return self;
+}
+
+-(instancetype)initWithVal:(int)value{
+    if(self){
+        self = [super init];
+        self.red = true;
+        self.storedValue = value;
+    }
+    return self;
+}
+
+@end
