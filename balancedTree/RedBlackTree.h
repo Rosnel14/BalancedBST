f626774bf0a66dfae986c27d476b956d1c15349a
JerJer2465 <jerryyuan20040605@gmail.com>
2021-05-10 09:37:38 -0400

    test

---
+++ balancedTree/RedBlackTree.h
@@ -0,0 +1,18 @@
+//
+//  RedBlackTree.h
+//  balancedTree
+//
+//  Created by zyuan22 on 5/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Node.h"
+@interface RedBlackTree : NSObject
+
+-(void) rotateLeft: (Node *) leftInput :(Node *)rightInput;
+
+-(void) rotateLeft: (Node *) leftInput :(Node *)rightInput;
+
+@end
+
---
+++ balancedTree/RedBlackTree.m
@@ -0,0 +1,12 @@
+//
+//  RedBlackTree.m
+//  balancedTree
+//
+//  Created by zyuan22 on 5/10/21.
+//
+
+#import "RedBlackTree.h"
+
+@implementation RedBlackTree
+
+@end
